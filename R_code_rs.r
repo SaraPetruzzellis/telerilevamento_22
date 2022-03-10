ac7bc598ffb7edfcbe77e5bb82f113bed1e4cb22
SaraPetruzzellis <101283160+SaraPetruzzellis@users.noreply.github.com>
2022-03-10 17:49:54 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,18 @@
 #Questo è il primo script che useremo a lezione
 
+#install.packages("raster")
 library(raster)
+
+#settaggio cartella
+setwd("C:/lab/")
+
+#import
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
+#plot function
+plot(l2011)
+
+#ColorRampPalette, decido una serie di colori e adatto un'immagine, ('c' indica una serie di dati)
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+plot(l2011, col=cl)
