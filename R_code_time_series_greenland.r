6e97ad95c844842a7a345debd4b1710c2b0541f9
SaraPetruzzellis <101283160+SaraPetruzzellis@users.noreply.github.com>
2022-04-07 17:46:44 +0200

    Update R_code_time_series_greenland.r

--- R_code_time_series_greenland.r
+++ R_code_time_series_greenland.r
@@ -33,3 +33,56 @@ plot(tgr, col=cl)
 plot(tgr[[1]], col=cl)
 
 plotRGB(tgr, r=1, g=2, b=3, stretch="lin")
+
+
+#time series of NO2 change in Europe during the lockdown
+library(raster)
+setwd("C:/lab/EN")
+
+en01 <- raster("EN_0001.png")
+en01
+cl <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(en01, col=cl)
+
+en13 <- raster("EN_0013.png")
+en13
+plot(en13, col=cl)
+
+#Let's import the whole set altogether
+
+#Import the whole set as in the Greenland example 
+# by the folloowing steps: 1. list.files, 2. lapply, 3. stack
+
+#1 List the files in a directory/folder
+rlist <- list.files(pattern = "EN")
+rlist
+
+#2 Apply a Function over a list of rasters
+lapply(rlist, raster)
+
+rimp <- lapply(rlist, raster)
+rimp
+
+#3 Stack vectors from a data frame or list 
+EN_stack <- stack(rimp)
+EN_stack
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100) 
+
+#plot everything
+plot(EN_stack, col=cl)
+
+# Plot EN01 besides EN13
+par(mfrow=c(1,2))
+plot(EN[[01]], col=cl)
+plot(EN[[13]], col=cl)
+
+#or
+en113 <- stack(EN[[01]], EN[[13]])
+plot(en113, col=cl)
+
+#make the difference
+difen <- EN[[01]] - EN[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100)
+plot(difen,col=cldif)
+
